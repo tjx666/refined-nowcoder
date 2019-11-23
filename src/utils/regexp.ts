2b53dc59c3610c4ff606bef30c3252f9dd6c35b7
YuTengjing <ytj2713151713@gmail.com>
2019-11-23 14:44:37 +0800

    :rotating_light: chore: fix some lint errors and optimze some detail

--- src/utils/regexp.ts
+++ src/utils/regexp.ts
@@ -1,3 +1,5 @@
+/* eslint-disable import/prefer-default-export */
+
 export const isValidRegexp = (strToBeValidated: string): boolean => {
     try {
         // eslint-disable-next-line no-new
--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,13 +1,13 @@
+/* eslint-disable import/prefer-default-export */
 const chromeSyncStorage = chrome.storage.sync;
-// const chromeLocalStorage = chrome.storage.local;
 
 const onlineStorage = {
-    get<T extends Object>(keys: T): Promise<Record<keyof T, any>> {
+    get<T extends Record<string, any>>(keys: T): Promise<Record<keyof T, any>> {
         return new Promise(resolve => {
             chromeSyncStorage.get(keys, items => resolve(items as Record<keyof T, any>));
         });
     },
-    set(keys: Object) {
+    set(keys: Record<string, any>) {
         return new Promise(resolve => {
             chromeSyncStorage.set(keys, () => resolve());
         });
